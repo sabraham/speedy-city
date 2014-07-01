350b15bbd12526b29e2e63ca5c0c1e63287fcfb4
Sunil Abraham <s.abraham@gmail.com>
2014-06-30 21:41:10 -0400

    9:43:13 PM, June 30, 2014

--- speedy/etl.sh
+++ speedy/etl.sh
@@ -4,7 +4,7 @@ do
     curl "http://207.251.86.229/nyc-links-cams/LinkSpeedQuery.txt" | csvcut --tabs -c "Speed","EncodedPolyLine" > speeds.csv
     echo $UPDATE > asof.txt
     git add -A .
-    git commit -m "$UPDATE"
-    git push origin master
+    git commit --amend -m "$UPDATE"
+    git push -f origin master
     sleep 30
 done
